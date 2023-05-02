69639415da17b978a8d5e7a6f9af93d6287fee32
Mille Nikita Alstrøm <mill9540@edu.zealand.dk>
2023-05-02 11:17:15 +0200

    Class creation: Item, CartItem and Order.

---
+++ Models/Order.cs
@@ -0,0 +1,23 @@
+﻿namespace CTTSite.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public decimal TotalPrice { get; set; }
+        public bool Shipped { get; set; }
+
+        public Order(int iD, int userID, decimal totalPrice, bool shipped)
+        {
+            ID=iD;
+            UserID=userID;
+            TotalPrice=totalPrice;
+            Shipped=shipped;
+        }
+
+        public Order()
+        {
+            
+        }
+    }
+}
