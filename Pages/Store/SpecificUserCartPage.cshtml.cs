026ac54f45da1fadf0a914fb060ee0420e3ee58d
Mille Nikita Alstrøm <mill9540@edu.zealand.dk>
2023-05-04 12:29:17 +0200

    Nav-bar cart page

---
+++ Pages/Store/SpecificUserCartPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CTTSite.Pages.Store
+{
+    public class SpecificUserCartPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
