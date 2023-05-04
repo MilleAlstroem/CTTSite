cac9bb347927cea13553ce0c3db1d56b813d7fe1
Canty <chri38eb@edu.zealand.dk>
2023-05-04 13:15:19 +0200

    mads wanted

--- Pages/Admin/UpdateUser.cshtml.cs
+++ Pages/Admin/UpdateUser.cshtml.cs
@@ -1,3 +1,4 @@
+using CTTSite.Services.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,6 +6,16 @@ namespace CTTSite.Pages.Admin
 {
     public class UpdateUserModel : PageModel
     {
+        private IUserService _userService;
+
+        public UpdateUserModel(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [BindProperty]
+        public Models.User User { get; set; }
+
         public void OnGet()
         {
         }
