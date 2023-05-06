5715f00b62f4d25c18086faf3ee0b4689aff617e
Canty <chri38eb@edu.zealand.dk>
2023-05-06 21:26:57 +0200

    forms

--- Models/Forms/Form.cs
+++ Models/Forms/Form.cs
@@ -1,8 +1,13 @@
-﻿namespace CTTSite.Models.Forms
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CTTSite.Models.Forms
 {
     public class Form
     {
         // Made by Christian
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
     }
 }
