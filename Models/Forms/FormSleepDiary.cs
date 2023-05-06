8a4334bbaa3302b49ab597ed06dc51d3f1cd0757
Canty <chri38eb@edu.zealand.dk>
2023-05-06 16:32:39 +0200

    Form Classes Created

---
+++ Models/Forms/FormSleepDiary.cs
@@ -0,0 +1,93 @@
+﻿namespace CTTSite.Models.Forms
+{
+    public class FormSleepDiary
+    {
+        public int Id { get; set; }
+
+        #region Day 1
+        public string Day1 { get; set; }
+        public string Day1_TimeInBed { get; set; }
+        public string Day1_HowLongToSleep { get; set; }
+        public string Day1_AmountOfTimesWokenUp { get; set; }
+        public string Day1_TotalTimeAwakeInNight { get; set; }
+        public string Day1_TimeWokenUpInMorning { get; set; }
+        public string Day1_TimeGottenOutOfBed { get; set; }
+        public string Day1_TotalTimeSpentInBed { get; set; }
+        public string Day1_TotalTimeSleeping { get; set; }
+        public string Day1_HowRestedDoYouFeel { get; set; }
+        #endregion
+        #region Day 2
+        public string Day2 { get; set; }
+        public string Day2_TimeInBed { get; set; }
+        public string Day2_HowLongToSleep { get; set; }
+        public string Day2_AmountOfTimesWokenUp { get; set; }
+        public string Day2_TotalTimeAwakeInNight { get; set; }
+        public string Day2_TimeWokenUpInMorning { get; set; }
+        public string Day2_TimeGottenOutOfBed { get; set; }
+        public string Day2_TotalTimeSpentInBed { get; set; }
+        public string Day2_TotalTimeSleeping { get; set; }
+        public string Day2_HowRestedDoYouFeel { get; set; }
+        #endregion
+        #region Day 3
+        public string Day3 { get; set; }
+        public string Day3_TimeInBed { get; set; }
+        public string Day3_HowLongToSleep { get; set; }
+        public string Day3_AmountOfTimesWokenUp { get; set; }
+        public string Day3_TotalTimeAwakeInNight { get; set; }
+        public string Day3_TimeWokenUpInMorning { get; set; }
+        public string Day3_TimeGottenOutOfBed { get; set; }
+        public string Day3_TotalTimeSpentInBed { get; set; }
+        public string Day3_TotalTimeSleeping { get; set; }
+        public string Day3_HowRestedDoYouFeel { get; set; }
+        #endregion
+        #region Day 4
+        public string Day4 { get; set; }
+        public string Day4_TimeInBed { get; set; }
+        public string Day4_HowLongToSleep { get; set; }
+        public string Day4_AmountOfTimesWokenUp { get; set; }
+        public string Day4_TotalTimeAwakeInNight { get; set; }
+        public string Day4_TimeWokenUpInMorning { get; set; }
+        public string Day4_TimeGottenOutOfBed { get; set; }
+        public string Day4_TotalTimeSpentInBed { get; set; }
+        public string Day4_TotalTimeSleeping { get; set; }
+        public string Day4_HowRestedDoYouFeel { get; set; }
+        #endregion
+        #region Day 5
+        public string Day5 { get; set; }
+        public string Day5_TimeInBed { get; set; }
+        public string Day5_HowLongToSleep { get; set; }
+        public string Day5_AmountOfTimesWokenUp { get; set; }
+        public string Day5_TotalTimeAwakeInNight { get; set; }
+        public string Day5_TimeWokenUpInMorning { get; set; }
+        public string Day5_TimeGottenOutOfBed { get; set; }
+        public string Day5_TotalTimeSpentInBed { get; set; }
+        public string Day5_TotalTimeSleeping { get; set; }
+        public string Day5_HowRestedDoYouFeel { get; set; }
+        #endregion
+        #region Day 6
+        public string Day6 { get; set; }
+        public string Day6_TimeInBed { get; set; }
+        public string Day6_HowLongToSleep { get; set; }
+        public string Day6_AmountOfTimesWokenUp { get; set; }
+        public string Day6_TotalTimeAwakeInNight { get; set; }
+        public string Day6_TimeWokenUpInMorning { get; set; }
+        public string Day6_TimeGottenOutOfBed { get; set; }
+        public string Day6_TotalTimeSpentInBed { get; set; }
+        public string Day6_TotalTimeSleeping { get; set; }
+        public string Day6_HowRestedDoYouFeel { get; set; }
+        #endregion
+        #region Day 7
+        public string Day7 { get; set; }
+        public string Day7_TimeInBed { get; set; }
+        public string Day7_HowLongToSleep { get; set; }
+        public string Day7_AmountOfTimesWokenUp { get; set; }
+        public string Day7_TotalTimeAwakeInNight { get; set; }
+        public string Day7_TimeWokenUpInMorning { get; set; }
+        public string Day7_TimeGottenOutOfBed { get; set; }
+        public string Day7_TotalTimeSpentInBed { get; set; }
+        public string Day7_TotalTimeSleeping { get; set; }
+        public string Day7_HowRestedDoYouFeel { get; set; }
+        #endregion
+
+    }
+}
