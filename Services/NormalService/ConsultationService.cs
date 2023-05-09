279b624902a79b145b05ffdcb31be8bb743cad04
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-05-09 12:10:28 +0200

    Creation of Consultation class, service and what is needed for program

---
+++ Services/NormalService/ConsultationService.cs
@@ -0,0 +1,37 @@
+﻿using CTTSite.Models;
+using CTTSite.Services.DB;
+using CTTSite.Services.Interface;
+
+namespace CTTSite.Services.NormalService
+{
+    public class ConsultationService : IConsultationService
+    {
+        public List<Consultation> Consultations;
+        public DBServiceGeneric<Consultation> DBServiceGeneric;
+
+        public void CreateConsultation(Consultation consultation)
+        {
+            
+        }
+
+        public void DeleteConsultation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Consultation> GetAllConsultations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetConsultation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateConsultation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
