bafed4eed8f0fa8644db2a6ac99a35a8426b30c0
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-05-09 23:31:45 +0200

    Update for branch

--- Pages/Consultaion/BookConsultationPage.cshtml.cs
+++ Pages/Consultaion/BookConsultationPage.cshtml.cs
@@ -8,13 +8,27 @@ namespace CTTSite.Pages.Consultation
     {
         public IConsultationService IConsultationService;
 
+        [BindProperty]
+        public Models.Consultation consultation;
+
         public BookConsultationPageModel(IConsultationService iConsultationService)
         {
             IConsultationService = iConsultationService;
         }
 
-        public void OnGet()
+        public void OnGet(int ID)
         {
+            consultation = IConsultationService.GetConsultationByID(ID);
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            IConsultationService.UpdateConsultation(consultation);
+            return RedirectToPage("GetAllConsultaionsPage");
         }
     }
 }
