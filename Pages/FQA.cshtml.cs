e98b28e73d9261e70defa9b9b2caf9752ee42509
c-canty <christian_canty@hotmail.co.uk>
2023-05-17 15:54:58 +0200

    FQA's

---
+++ Pages/FQA.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CTTSite.Pages
+{
+    public class FQAModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
