787a5307f85b7410cfe3cc4c290d44c226b5e66b
c-canty <christian_canty@hotmail.co.uk>
2023-05-17 18:31:18 +0200

    done for the day

--- Pages/User/LogIn/EditUserDetails.cshtml.cs
+++ Pages/User/LogIn/EditUserDetails.cshtml.cs
@@ -19,6 +19,7 @@ namespace CTTSite.Pages.User.LogIn
 		public string? Message { get; set; }
 
 
+
 		public Models.User? user { get; set; }
 
 		[BindProperty]
@@ -27,21 +28,54 @@ namespace CTTSite.Pages.User.LogIn
 		[BindProperty, DataType(DataType.Password)]
 		public string? Password { get; set; }
 
-		public IActionResult OnGet()
+        [BindProperty, DataType(DataType.Password)]
+        public string? PasswordCheck { get; set; }
+
+        public IActionResult OnGet()
 		{
 			user = _userService.GetUserByEmail(HttpContext.User.Identity.Name);
 			return Page();
 		}
 
 		public IActionResult OnPost(int id)
-		{
+		{            
+            if (Password == null)
+            {
+                return RedirectToPage("PasswordError");
+            }
 
+            bool containsUppercase = false;
 
-			if (!ModelState.IsValid)
-			{
-				Message = "Something Went Wrong. Please Try Again";
-				return Page();
-			}
+            foreach (char c in Password)
+            {
+                if (char.IsUpper(c))
+                {
+                    containsUppercase = true;
+                    break;
+                }
+            }           
+
+            if ((Password.Length < 6) && (!containsUppercase))
+            {
+                return RedirectToPage("PasswordError");
+            }        
+
+            if (!containsUppercase)
+            {
+                return RedirectToPage("PasswordError");
+            }
+
+            if (Password.Length < 6)
+            {
+                return RedirectToPage("PasswordError");
+            }
+
+            if(Password != PasswordCheck)
+            {                
+                return RedirectToPage("PasswordError");
+            }   
+           
+
 
 			passwordHasher = new PasswordHasher<string>();
             user = _userService.GetUserByEmail(HttpContext.User.Identity.Name);
---
+++ Pages/User/LogIn/PasswordError.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CTTSite.Pages.User.LogIn
+{
+    public class PasswordErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
