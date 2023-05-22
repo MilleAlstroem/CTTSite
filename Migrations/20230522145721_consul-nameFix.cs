965de1ce063607d16864329e3692b261efbaf555
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-05-22 16:58:15 +0200

    update db

---
+++ Migrations/20230522145721_consul-nameFix.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CTTSite.Migrations
+{
+    /// <inheritdoc />
+    public partial class consulnameFix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TelefonNummer",
+                table: "Consultations",
+                newName: "TelefonNumber");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TelefonNumber",
+                table: "Consultations",
+                newName: "TelefonNummer");
+        }
+    }
+}
